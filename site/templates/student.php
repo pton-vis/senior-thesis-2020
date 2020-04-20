f7f08f835c663dcfc924dd04e382b463f35de242
Eric Li <ericyoungli@gmail.com>
2020-04-19 20:52:57 -0400

    typos

--- site/templates/student.php
+++ site/templates/student.php
@@ -24,7 +24,6 @@
   $hasBgImg = $page->background_image_or_color() == 'image';
   $hasBgColor = $page->background_image_or_color() == 'color';
 ?>
-
 <?php if ($page->content_or_url() == 'content'): ?>
   <div
     class="
